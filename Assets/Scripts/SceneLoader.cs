9955294a8a307c6f5c449d82a4841620d159d345
mirosal <mirosalminen89@gmail.com>
2021-01-31 14:11:42 +0200

    BingoStudio UI is moved to separate scene that is loaded on top of BingoStudio scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script for sceneloading
+/// If loaded as additive, scene is loaded on top of other open scenes.
+/// </summary>
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField]
-    private string MainScenename = "BingoMain";
+    private string sceneName = "";
+
+    [SerializeField]
+    private bool bLoadAsAdditiveScene = false;
 
     void Start()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(MainScenename);
+        if (bLoadAsAdditiveScene)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
     }
 }
